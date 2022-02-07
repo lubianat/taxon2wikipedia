22f379e881de8d90930d5aa754256ad9e78f80f5
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-02-07 09:59:52 -0300

    Add more information to automatic page

--- src/base.py
+++ src/base.py
@@ -23,7 +23,7 @@ results = sparql.query().convert()
 
 
 results_df = pd.io.json.json_normalize(results['results']['bindings'])
-print(results_df)
+print(results_df.columns)
 
 
 taxon_rank_label = results_df["taxon_rankLabel.value"][0]
@@ -31,14 +31,19 @@ parent_taxon_label = results_df["parent_taxonLabel.value"][0]
 parent_taxon = results_df["parent_taxon.value"][0]
 taxon_name = results_df["taxon_name.value"][0]
 
+taxon_author_label = results_df["taxon_authorLabel.value"][0]
+description_year = results_df["description_year.value"][0]
+
+
+
 print(f"""
 
-''''{taxon_name} ''''' is a species of the group [[{parent_taxon_label}]].
+'''''{taxon_name}''''' é uma espécie do grupo [[{parent_taxon_label}]] decrita em [[{description_year}]] por [[{taxon_author_label}]].
 
+{{{{Referencias}}}}
 
-
-
-
-
+{{{{esboço}}}}
 
 """)
+
+print(f"""https://pt.wikipedia.org/wiki/{taxon_name.replace(" ", "_")}?veaction=edit""")
