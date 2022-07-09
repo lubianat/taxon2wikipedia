50a11e09af3fe1c0a9ccac94f34ace4fa5760ec3
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-07-09 09:27:17 -0300

    Refactor and update dict

--- src/taxon2wikipedia/render_page_espirito_santo.py
+++ src/taxon2wikipedia/render_page_espirito_santo.py
@@ -1,75 +1,22 @@
 #!/usr/bin/env python3
 
-from SPARQLWrapper import SPARQLWrapper, JSON
-import pandas as pd
-from jinja2 import Template
-import sys
-from .qid2taxobox import *
-from .cleanup import *
-import pywikibot
-from pathlib import Path
-from .process_reflora import *
-import click
-import webbrowser
-from wdcuration import render_qs_url
-from pywikibot.page import Page
-from urllib3.exceptions import InsecureRequestWarning
-from urllib3 import disable_warnings
-
-disable_warnings(InsecureRequestWarning)
-
-HERE = Path(__file__).parent.resolve()
-
-
-def get_gbif_ref(qid):
-    query = f"""
-    SELECT * WHERE {{ 
-        wd:{qid} wdt:P846 ?gbif_id .
-        wd:{qid} wdt:P225 ?taxon_name . }}
-    """
-    df = get_rough_df_from_wikidata(query)
-    if "gbif_id.value" not in df:
-        return ""
-    gbif_id = list(df["gbif_id.value"])[0]
-    taxon_name = list(df["taxon_name.value"])[0]
-    ref = f"""<ref>{{{{Citar web|url=https://www.gbif.org/species/{gbif_id}|titulo={taxon_name}|acessodata=2022-04-18|website=www.gbif.org|lingua=en}}}}</ref>"""
-    return ref
-
-
-def render_taxonomy(reflora_data, results_df, qid):
-
-    if "taxon_authorLabel.value" not in results_df:
-        description = ""
-    else:
-        taxon_author_labels = results_df["taxon_authorLabel.value"].values
-        description_year = results_df["description_year.value"][0]
-
-        taxon_author_labels = [f"[[{name}]]" for name in taxon_author_labels]
-        description = f"A espécie foi descrita em [[{description_year}]] por {render_list_without_dict(taxon_author_labels)}. {get_gbif_ref(qid)}"
-
-    text = f"""== Taxonomia ==
-{description}
-{get_subspecies_from_reflora(reflora_data)}
-{get_synonyms_from_reflora(reflora_data)}"""
-
-    return text
+from .helper import *
 
 
 @click.command(name="render")
 @click.option("--qid")
 @click.option("--scope-name", default="planta", help="O escopo do táxon alvo.")
 @click.option("--reflora-id", default="search", help="O número do taxon na base Reflora.")
-def main(scope_name: str, qid: str, reflora_id: str):
+@click.option("--open_url", default=False, help="Abrir ou não as páginas auxiliares")
+def main(scope_name: str, qid: str, reflora_id: str, open_url: bool):
 
     template_path = Path(f"{HERE}/../data/full_query_taxon.rq.jinja")
     t = Template(template_path.read_text())
     query = t.render(taxon=qid)
 
     results_df = get_rough_df_from_wikidata(query)
-
     parent_taxon_df = get_parent_taxon_df(qid)
-    print(parent_taxon_df)
-    print(parent_taxon_df["taxonRankLabel.value"])
+
     family = parent_taxon_df["taxonName.value"][
         parent_taxon_df["taxonRankLabel.value"] == "família"
     ].item()
@@ -85,10 +32,13 @@ def main(scope_name: str, qid: str, reflora_id: str):
         year_cat = f"[[Categoria:Plantas descritas em {description_year}]]"
 
     reflora_url = f"""http://servicos.jbrj.gov.br/flora/search/{taxon_name.replace(" ", "_")}"""
-    webbrowser.open(
-        f"""https://scholar.google.com/scholar?q=%22{taxon_name.replace(" ", "+")}%22+scielo"""
-    )
-    webbrowser.open(f"""https://google.com/search?q=%22{taxon_name.replace(" ", "+")}%22""")
+
+    if open_url:
+        webbrowser.open(
+            f"""https://scholar.google.com/scholar?q=%22{taxon_name.replace(" ", "+")}%22+scielo"""
+        )
+        webbrowser.open(f"""https://google.com/search?q=%22{taxon_name.replace(" ", "+")}%22""")
+
     if reflora_id == "search":
         r = requests.get(reflora_url, verify=False)
         webbrowser.open(reflora_url)
@@ -204,14 +154,6 @@ A espécie faz parte da [[Lista Vermelha da IUCN|Lista Vermelha]] das espécies 
     stringclaim = pywikibot.Claim(repo, "P10701")  # Else, add the value
     stringclaim.setTarget(f"FB{str(reflora_id)}")
     item.addClaim(stringclaim, summary="Adding a Reflora ID")
-
-
-def render_common_name(results_df):
-    try:
-        common_name = results_df["taxon_common_name_pt.value"][0]
-        return f""", também conhecido como '''{common_name}''',"""
-    except:
-        return ""
 
 
 if __name__ == "__main__":
