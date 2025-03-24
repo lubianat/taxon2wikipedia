13971e7c05264c9e984cf8f378514ba41342b12f
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-03-24 17:09:04 -0300

    test: Create basic test for Spanish and Portuguese

--- src/tests/test_render_page.py
+++ src/tests/test_render_page.py
@@ -1,61 +1,91 @@
 import re
-from taxon2wikipedia.render_page import *
-
-qid = "Q15514411"
-language = "en"
-
-
-def run_pipeline(qid, language):
-    new_qid = check_if_is_basionym(qid)
-    if new_qid:
-        print("This is a basionym. Do you want to proceed with the accepted name or your input?")
-        print("Old QID: ", qid, "(https://www.wikidata.org/wiki/" + qid + ")")
-        print("New QID: ", new_qid, "(https://www.wikidata.org/wiki/" + new_qid + ")")
-        selection = input("Accepted name (a) or Basionym (b)?")
-        if selection.lower() == "a":
-            qid = new_qid
-
-    results_df = get_results_dataframe_from_wikidata(qid)
-    taxon_name = results_df["taxon_name.value"][0]
-
-    parent_taxon_df = get_parent_taxon_df(qid, language)
-    kingdom_name = get_kingdom_name(parent_taxon_df)
-    family_name = get_family_name(parent_taxon_df)
-    class_name = get_class_name(parent_taxon_df)
-    genus_name = get_genus_name(parent_taxon_df)
-    year_category = get_year_category(results_df, language)
-
-    wiki_page = get_wiki_page(
-        qid,
-        taxon_name,
-        results_df,
-        kingdom_name,
-        class_name,
-        family_name,
-        genus_name,
-        year_category,
-        language,
-    )
-    return wiki_page
+from taxon2wikipedia.render_page import run_pipeline_for_wikipage
 
 
 # Test if Wikipage is a string
 
 
-def test_wiki_page_is_string():
-    assert isinstance(wiki_page, str)
-
-
 def test_wikipage_in_portuguese():
-    wiki_page = get_wiki_page(
-        qid,
-        taxon_name,
-        results_df,
-        kingdom_name,
-        class_name,
-        family_name,
-        genus_name,
-        year_category,
-        "pt",
-    )
-    assert "pt.wikipedia.org" in wiki_page
+    wiki_page = run_pipeline_for_wikipage("Q15514411", "pt")
+
+    expected_page = """
+{{Título em itálico}}
+{{Info/Taxonomia
+| imagem                = 
+| reino                 = [[Plantae]]
+| divisão               = [[Tracheophytes]]
+| ordem                 = [[Poales]]
+| família               = [[Poaceae]]
+| infrarreino           = [[Streptophyta]]
+| superdivisão          = [[Embryophytes]]
+| subfilo               = [[Euphyllophyta]]
+| superdomínio          = [[Biota]]
+| género                = [[Cutandia]]
+| superdomínio          = [[Cytota]]
+| subdomínio            = [[Diaphoretickes]]
+| subreino              = [[Viridiplantae]]
+| espécie               = [[Cutandia divaricata]]
+}}
+'''''Cutandia divaricata''''' é uma espécie de planta  da família [[Poaceae]] e do gênero ''[[Cutandia]]''.  <ref name=":ref_0"> {{Citar web|url=https://www.gbif.org/species/4140116|titulo=Cutandia divaricata|acessodata=2022-04-18|website=www.gbif.org|lingua=en}}</ref>
+== Taxonomia ==
+O táxon foi descrito oficialmente em [[1881]] por [[George Bentham]]. <ref name=":ref_0"/>
+A espécie havia sido descrita anteriormente sob o [[basiônimo]] '''''Festuca divaricata''''' (gênero ''[[Festuca]]'') em 1798 por [[René Louiche Desfontaines]]. 
+{{Referencias}}
+== Ligações externas ==
+* [https://www.ipni.org/n/396840-1 ''Cutandia divaricata'' no Índice Internacional de Nomes de Plantas.]
+* [https://www.biodiversitylibrary.org/name/Cutandia%20divaricata Documentos sobre ''Cutandia divaricata'' na Biodiversity Heritage Library]
+* [https://www.inaturalist.org/taxa/1159197 Observações de ''Cutandia divaricata'' no iNaturalist]
+* [https://www.gbif.org/species/4140116 ''Cutandia divaricata'' no GBIF]
+   
+=== Links externos para sinônimos ===
+* [https://www.biodiversitylibrary.org/name/Festuca%20divaricata Documentos sobre ''Festuca divaricata'' na Biodiversity Heritage Library]
+
+
+{{Controle de autoridade}}
+{{esboço-biologia}}
+[[Categoria:Cutandia]][[Categoria:Espécies descritas em 1881]]"""
+
+    assert wiki_page == expected_page
+
+
+def test_wikipage_in_spanish():
+    wiki_page = run_pipeline_for_wikipage("Q15514411", "es")
+
+    expected_page = """
+{{Ficha de taxón
+| imagem                = 
+| regnum                 = [[Plantae]]
+| unranked_divisio           = [[Spermatophytes]]
+| divisio              = [[Tracheophytes]]
+| ordo                 = [[Poales]]
+| familia               = [[Poaceae]]
+| subregnum           = [[Streptophyta]]
+| superdivisio         = [[Embryophytes]]
+| subphylum               = [[Euphyllophyta]]
+| superregnum          = [[Biota]]
+| genus                = [[Cutandia]]
+| superregnum          = [[Cytota]]
+| subregnum            = [[Diaphoretickes]]
+| subregnum              = [[Viridiplantae]]
+| species               = [[Cutandia divaricata]]
+}}
+'''''Cutandia divaricata''''' es una especie de planta de la familia [[Poaceae]] y del género ''[[Cutandia]]''.  <ref name=":ref_0"> {{Citar web|url=https://www.gbif.org/species/4140116|título=Cutandia divaricata|fechaacceso=2022-04-18|sitioweb=www.gbif.org}}</ref>
+== Taxonomía ==
+El taxón fue descrito oficialmente en [[1881]] por [[George Bentham]]. <ref name=":ref_0"/>
+La especie había sido descrita anteriormente bajo el [[basónimo]] '''''Festuca divaricata''''' (género ''[[Festuca]]'') en 1798 por [[René Louiche Desfontaines]]. 
+==Referencias==
+{{listaref}}
+== Enlaces externos ==
+* [https://www.ipni.org/n/396840-1 ''Cutandia divaricata'' en el Índice Internacional de Nombres de Plantas.]
+* [https://www.biodiversitylibrary.org/name/Cutandia%20divaricata Documentos sobre ''Cutandia divaricata'' en la Biodiversity Heritage Library]
+* [https://www.inaturalist.org/taxa/1159197 Observaciones de ''Cutandia divaricata'' en iNaturalist]
+* [https://www.gbif.org/species/4140116 ''Cutandia divaricata'' en GBIF]
+   
+=== Enlaces externos para sinónimos ===
+* [https://www.biodiversitylibrary.org/name/Festuca%20divaricata Documentos sobre ''Festuca divaricata'' en la Biodiversity Heritage Library]
+
+
+{{Control de autoridad}}
+[[Categoría:Cutandia]][[Categoría:Especies descritas en 1881]]"""
+
+    assert wiki_page == expected_page
